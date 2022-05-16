0b525d9dcbf3cd77db539668c07f9964a17b98ff
Kaio Militão <kaioaugustoalmeida.m@gmail.com>
2022-05-16 15:20:01 -0300

    feat: adiciona validação para criar produtos e ajustes no middleware

--- middlewares/helpers/httpStatusCode.js
+++ middlewares/helpers/httpStatusCode.js
@@ -3,11 +3,21 @@ function httpStatus(type) {
     const Code = {
         ok: 200,
         notFound: 404,
+        badRequest: 400,
+        unprocessableEntity: 422,
     };
 
     return Code[type];
 }
 
+const httpStatusCheck = (errorType) => {
+    if (errorType === 'number.min' || errorType === 'string.min') {
+        return httpStatus('unprocessableEntity');
+    } 
+    if (errorType === 'any.required') return httpStatus('badRequest');
+};
+
 module.exports = {
     httpStatus,
+    httpStatusCheck,
 };
--- middlewares/productsValidation.js
+++ middlewares/productsValidation.js
@@ -1,6 +1,20 @@
+const Joi = require('joi');
 const Product = require('../services/productService');
 const { errorMessage } = require('./helpers/errorMessages');
-const { httpStatus } = require('./helpers/httpStatusCode');
+const { httpStatus, httpStatusCheck } = require('./helpers/httpStatusCode');
+
+const PRODUCT = Joi.object({
+    name: Joi.string().min(5).max(30).required()
+    .messages({
+        'string.min': '"name" length must be at least 5 characters long',
+        'any.required': '"name" is required',
+    }),
+    quantity: Joi.number().min(1).required()
+    .messages({
+        'number.min': '"quantity" must be greater than or equal to 1',
+        'any.required': '"quantity" is required',
+    }),
+});
 
 const getProductValidation = async (req, res, next) => {
     const { id } = req.params;
@@ -10,6 +24,20 @@ const getProductValidation = async (req, res, next) => {
     next();
 };
 
+const createProductValidation = async (req, res, next) => {
+    const { name, quantity } = req.body;
+    const { error } = PRODUCT.validate({ name, quantity });
+
+    if (error) {
+        const errorType = error.details[0].type;
+        
+        return res.status(httpStatusCheck(errorType)).json({ message: error.message });
+    }
+
+    next();
+};
+
 module.exports = {
     getProductValidation,
+    createProductValidation,
 };
