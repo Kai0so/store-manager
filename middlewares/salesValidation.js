f8fff8b161bba7294cc4ee312e3c38e9a121b1ea
Kaio Militão <kaioaugustoalmeida.m@gmail.com>
2022-05-19 02:52:30 -0300

    feat: adiciona validação para criar vendas

--- middlewares/salesValidation.js
+++ middlewares/salesValidation.js
@@ -1,6 +1,19 @@
+const Joi = require('joi');
 const Sale = require('../services/saleService');
 const { errorMessage } = require('./helpers/errorMessages');
-const { httpStatus } = require('./helpers/httpStatusCode');
+const { httpStatus, httpStatusCheck } = require('./helpers/httpStatusCode');
+
+const SALE = Joi.object({
+    quantity: Joi.number().min(1).required()
+        .messages({
+            'number.min': '"quantity" must be greater than or equal to 1',
+            'any.required': '"quantity" is required',
+        }),
+    productId: Joi.number().required()
+        .messages({
+            'any.required': '"productId" is required',
+        }),
+});
 
 const getSaleValidation = async (req, res, next) => {
     const { id } = req.params;
@@ -12,6 +25,22 @@ const getSaleValidation = async (req, res, next) => {
     next();
 };
 
+const createSaleValidation = async (req, res, next) => {
+    const sales = req.body;
+    
+    sales.forEach((s) => {
+        const { productId, quantity } = s;
+        const { error } = SALE.validate({ productId, quantity });
+        if (error) {
+            const errType = error.details[0].type;
+            const errMessage = error.details[0].message;
+            next(res.status(httpStatusCheck(errType)).json({ message: errMessage }));
+        }
+    });
+    next();
+};
+
 module.exports = {
     getSaleValidation,
+    createSaleValidation,
 };
