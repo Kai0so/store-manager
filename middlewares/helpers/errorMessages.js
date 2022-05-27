08ff15021c6a0cde8f4cac9ddc836f3c7ed2cef6
Kaio Militão <kaioaugustoalmeida.m@gmail.com>
2022-05-27 18:24:19 -0300

    fix: Adiciona ajustes nos helpers

--- middlewares/helpers/errorMessages.js
+++ middlewares/helpers/errorMessages.js
@@ -3,6 +3,7 @@ function errorMessage(type) {
     const Message = {
         notFoundProduct: { message: 'Product not found' },
         notFoundSale: { message: 'Sale not found' },
+        createProductConflict: { message: 'Product already exists' },
     };
 
     return Message[type];
--- middlewares/helpers/httpStatusCode.js
+++ middlewares/helpers/httpStatusCode.js
@@ -2,9 +2,11 @@
 function httpStatus(type) {
     const Code = {
         ok: 200,
+        created: 201,
         noContent: 204,
-        notFound: 404,
         badRequest: 400,
+        notFound: 404,
+        conflict: 409,
         unprocessableEntity: 422,
     };
 
