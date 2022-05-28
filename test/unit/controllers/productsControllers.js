25014e70aeed60333590214f715f31e3702f0785
Kaio Militão <kaioaugustoalmeida.m@gmail.com>
2022-05-28 11:54:26 -0300

    feat: Adiciona 40% da cobertura dos testes

---
+++ test/unit/controllers/productsControllers.js
@@ -0,0 +1,36 @@
+const { expect } = require('chai');
+const sinon = require('sinon');
+
+const productController = require('../../../controllers/productController');
+const pŕoductService = require('../../../services/productService');
+
+describe('productController', () => {
+  describe('Quando é invocada a função getAll', () => {
+    const request = {}
+    const response = {}
+    const getAllResult = [];
+
+    before(() => {
+      sinon.stub(pŕoductService, 'getAll').returns(getAllResult);
+
+      response.status = sinon.stub().returns(response);
+      response.json = sinon.stub().returns();
+    });
+
+    after(() => {
+      pŕoductService.getAll.restore();
+    });
+
+    it('Retorna status 200', async () => {
+      await productController.getAll(request, response);
+
+      expect(response.status.calledWith(200)).to.be.equal(true);
+    });
+
+    it('Retorna um JSON com todos os produtos', async () => {
+      await productController.getAll(request, response);
+      expect(response.json.calledWith(getAllResult)).to.be.equal(true);
+    });
+
+  });
+});
---
+++ test/unit/models/productsModels.js
@@ -0,0 +1,29 @@
+const { expect } = require('chai');
+const sinon = require('sinon');
+const productModel = require('../../../models/productModel');
+const connection = require('../../../models/connection');
+const { getAllResult } = require('../mocks/products');
+
+describe('productModel', () => {
+    describe('Quando é invocada a função getAll', () => {
+
+        before(() => {
+            sinon.stub(connection, 'execute').resolves(getAllResult)
+        })
+
+        after(() => {
+            connection.execute.restore();
+        })
+
+        it('retorna um array', async () => {
+            const result = await productModel.getAll();
+            expect(result).to.be.an('array');
+        })
+
+        it('o array não está vazio', async () => {
+            const result = await productModel.getAll();
+
+            expect(result).to.be.not.empty;
+        })
+    });
+});
